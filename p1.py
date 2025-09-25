7d264e36bf88909f70a2f6b595c295fd6e20c097
Suyash Singh <suyxcode@gmail.com>
2025-09-26 01:10:57 +0530

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,24 @@
+# HCF of Two Numbers
+#Here, in this section we will discuss how to find HCF of two numbers in python. HCF means (Highest Common Factor) also known as GCD (Greatest Common Divisor).
+
+#x is called HCF of a & b two conditions :
+
+#x can completely divide both a & b leaving remainder 0
+#No, other number greater than x can completely divide both a & b
+#Linear Quest to find HCF
+
+# Algorithm
+
+# Initialize HCF = 1
+# Run a loop in the iteration of (i) between [1, min(num1, num2)]
+# Note down the highest number that divides both num1 & num2
+# If i satisfies (num1 % i == 0 and num2 % i == 0) then new value of HCF is i
+# Print value of HCF
+num1 = 36
+num2 = 60
+hcf = 1
+
+for i in range(1, min(num1, num2)):
+    if num1 % i == 0 and num2 % i == 0:
+        hcf = i
+print("Hcf of", num1, "and", num2, "is", hcf)
