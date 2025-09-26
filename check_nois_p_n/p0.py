c2cfd1dc9a892855d7b36b285e0f1f6dfc468b15
suyXcode <suyxcode@gmail.com>
2025-09-26 15:09:54 +0530

    Added p0.py in check_nois_p_n subfolder

---
+++ check_nois_p_n/p0.py
@@ -0,0 +1,9 @@
+#Check if a Number is Positive and Negative in Python
+#   Using Brute Force
+n=int(input("Enter a number: "))
+if n>0:
+    print(f"{n} is a Positive number.")
+elif n<0:
+    print(f"{n} is a Negative number.")
+else:
+    print("The number is Zero.")
