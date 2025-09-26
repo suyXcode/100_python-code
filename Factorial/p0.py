b6e70743626093e263806cd37eaffce5ead955f0
suyXcode <suyxcode@gmail.com>
2025-09-26 14:34:50 +0530

    Added p0.py in Factorial subfolder

--- Factorial/p0.py
+++ Factorial/p0.py
@@ -1,1 +1,7 @@
-# factorial of n natural numbers
+#Factorial of given number
+
+n=int(input("Enter a number: "))
+fact=1
+for i in range(1,n+1):
+    fact = fact * i
+print(f"Factorial of {n} is {fact}.")
