f4c857f8736710efdc6c6ed98fc68b2931ed5e6f
suyXcode <suyxcode@gmail.com>
2025-09-27 00:27:44 +0530

    Added p1.py in check_nois_p_n .

---
+++ check_nois_p_n/p2.py
@@ -0,0 +1,12 @@
+'''
+Using Ternary Operator
+This method uses a ternary operator to check whether a number is Positive or Negative.
+
+Ternary Operator Syntax
+
+    ( Condition ) ? ( if True : Action) : ( if False : Action) ;
+
+'''
+
+num =15
+print("Positive" if num>=0 else "Negative")
