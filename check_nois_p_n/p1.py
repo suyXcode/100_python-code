774954e54e0595654e86e52a68621608158cc46c
suyXcode <suyxcode@gmail.com>
2025-09-27 00:20:59 +0530

    Updated code

--- check_nois_p_n/p1.py
+++ check_nois_p_n/p1.py
@@ -1,6 +1,24 @@
 # Method 2: Using Nested if-else Statements
 # This method uses a nested if-else Statements to check whether a given number is Positive or Negative.
 
+"""
+Algorithm
+-----------------
+
+This method uses a nested if-else Statements to check whether a given number is Positive or Negative.
+
+Algorithm for the above code is as follows,
+
+1. Initialize num as 15
+2. If the num >= 0
+        If num == 0 : num is zero
+        Else number has to be positive
+3. Else the number has to be negative
+"""
+
+
+
+
 num = 15
 if num>=0:
     if num==0:
