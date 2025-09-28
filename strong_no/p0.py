2a24a99fb9a74211ac4750757929f3d9a0694eaa
suyXcode <suyxcode@gmail.com>
2025-09-29 02:20:32 +0530

    Added p0.py in strong number subfolder.

---
+++ strong_no/p0.py
@@ -0,0 +1,16 @@
+# 25.Wap to check the no. is a strong no. or not.
+n=int(input("Enter The int num:"))
+sum=0
+a=n
+while n>0:
+    ld=n%10
+    fact=1
+    while ld>0:
+        fact*=ld
+        ld-=1
+    sum+=fact
+    n//=10
+if sum==a:
+    print(a,"is a strong number")
+else:
+    print(a,"is not a strong number")
