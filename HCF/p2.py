d1e5adc1e22305a97a258206030710cc455143b7
suyXcode <suyxcode@gmail.com>
2025-09-28 11:40:36 +0530

    Added p2.py in HCF subfolder.

---
+++ HCF/p2.py
@@ -0,0 +1,18 @@
+#Repeated Subtraction with Modulo Operator using Recursion
+'''
+Algorithm
+------------
+If b is equals to 0 return a
+Else recursively call the function for value b, a%b and return 
+
+'''
+# This method improves complexity of repeated subtraction
+# By efficient use of modulo operator in euclidean algorithm
+
+def getGCD(a, b):
+    return b == 0 and a or getGCD(b, a % b)
+
+
+num1 = 36
+num2 = 60
+
