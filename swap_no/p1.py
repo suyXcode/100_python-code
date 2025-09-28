2fb0f7499d9807ca9c60820070e06533eb7a46db
suyXcode <suyxcode@gmail.com>
2025-09-28 11:57:38 +0530

    Added p1.py in swap_no subfolder.

---
+++ swap_no/p1.py
@@ -0,0 +1,9 @@
+#Swaping two numbers without using third variable
+
+a = 5
+b = 10
+print("Before swapping: a =", a, "b =", b)  
+a=a + b
+b=a - b
+a=a - b
+print("After swapping: a =", a, "b =", b)
